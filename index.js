cf60bc1eb00ead63d3035cc335d9919a74d7e35d
Prerna <prerna1998.ps@gmail.com>
2020-08-07 17:55:04 +0530

    Binding to alternate host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const ejs = require('ejs');
 
-const hostname = 'localhost';
+const hostname = '0.0.0.0';
 const port =  process.env.PORT || 3000;
 
 const app = express();
